867d93ee1207bdf54e386c13c4c4c474d8d917d4
Holyhoppsan <daniel.hall@live.se>
2020-08-25 23:07:18 +0100

    Completed tutorial

--- 2.4-Saving-to-a-database/index.js
+++ 2.4-Saving-to-a-database/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
 
 app.listen(3000, () => console.log('listening at 3000'));
@@ -8,19 +9,21 @@ app.use(express.json({
     limit: '1mb'
 }));
 
-const database = [];
+const database = new Datastore('database.db');
+database.loadDatabase();
 
 app.post('/api', (request, response) => {
     console.log('I got a request');
     console.log(request.body);
 
     const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
 
-    database.push(data);
-    console.log(database);
-
+    database.insert(data);
     response.json({
         status: 'success',
+        timestamp: timestamp,
         latitiude: data.lat,
         longitude: data.long
     });
