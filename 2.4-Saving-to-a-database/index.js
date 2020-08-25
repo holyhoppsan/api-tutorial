8d99738bc2b81f89f8194b4f76a8ef1994243b3b
Holyhoppsan <daniel.hall@live.se>
2020-08-25 22:53:18 +0100

    Added database local variable to index.js to serve as an intermediary database

--- 2.4-Saving-to-a-database/index.js
+++ 2.4-Saving-to-a-database/index.js
@@ -8,11 +8,17 @@ app.use(express.json({
     limit: '1mb'
 }));
 
+const database = [];
+
 app.post('/api', (request, response) => {
     console.log('I got a request');
     console.log(request.body);
 
     const data = request.body;
+
+    database.push(data);
+    console.log(database);
+
     response.json({
         status: 'success',
         latitiude: data.lat,
