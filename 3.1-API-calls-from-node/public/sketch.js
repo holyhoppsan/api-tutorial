6072b0f3ffd663cb3c8060b096a852e7bcf9fc84
Holyhoppsan <daniel.hall@live.se>
2020-08-31 17:45:22 +0100

    Added parsing of the response

--- 3.1-API-calls-from-node/public/sketch.js
+++ 3.1-API-calls-from-node/public/sketch.js
@@ -8,9 +8,21 @@ if ('geolocation' in navigator) {
 
         const api_url = `weather/${lat},${long}`;
         const response = await fetch(api_url);
-        const json = response.json();
-        console.log(json);
 
+        response.json().then(data => {
+            console.log(data);
+            const content = document.createElement('p');
+            var nameNode = document.createTextNode(`${data.name}`);
+            content.appendChild(nameNode);
+
+            var linebreak = document.createElement('br');
+            content.appendChild(linebreak);
+
+            var tempNode = document.createTextNode(`Temp: ${data.main.temp}`);
+            content.appendChild(tempNode);
+
+            document.getElementById('weather').append(content);
+        });
     });
 } else {
     console.log('geolocation not available');
