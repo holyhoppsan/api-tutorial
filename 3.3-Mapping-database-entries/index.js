47aa0c8409bf9dd5d65dd37a675a41de51871ed4
Holyhoppsan <daniel.hall@live.se>
2020-09-01 23:01:56 +0100

    Port from environment variable

--- 3.3-Mapping-database-entries/index.js
+++ 3.3-Mapping-database-entries/index.js
@@ -9,7 +9,8 @@ const {
 } = require('express');
 const app = express();
 
-app.listen(3000, () => console.log('listening at 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`starting server at ${port}`));
 
 app.use(express.static('public'));
 app.use(express.json({
